a3f237b4b2e009652b75c3dd8c386eb37fa0b786
shura <shura1991@gmail.com>
2010-06-19 08:49:39 +0200

    developing homepage

--- index.php
+++ index.php
@@ -1,5 +1,30 @@
-<pre>
 <?php
-print_r ($_GET);
+error_reporting (0);
+include_once ('UrlShortner.class.php');
+include_once ('config.php');
+
+if (array_key_exists ("redirect", $_GET))
+{
+    try
+    {
+        $db = new UrlShortner ($database);
+        header ("Location: " . $db->get ($_GET['redirect']));
+    }
+    catch (Exception $e)
+    {
+        die ($e->getMessage ());
+    }
+}
+else if (array_key_exists ("preview", $_GET))
+{
+    try
+    {
+        $db = new UrlShortner ($database);
+        echo $db->get ($_GET['preview']);
+    }
+    catch (Exception $e)
+    {
+        die ($e->getMessage ());
+    }
+}
 ?>
-</pre>
